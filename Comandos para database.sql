345ed7c84d4266e1d565bac97ce776b3d59cd01b
Anderson <loonander@gmail.com>
2024-05-14 17:38:47 -0300

    migração para postgress

---
+++ Comandos para database.sql
@@ -0,0 +1,15 @@
+
+select * from exemplos;
+select * from adms;
+select * from users;
+select * from hashtags;
+select * from projects;
+select * from blacklists;
+select * from project_hashtags;
+drop table exemplos;
+drop table adms;
+drop table projects;
+drop table blacklists;
+drop table project_hashtags;
+drop table hashtags;
+drop table users;
